d72e68ad33a5fefedc7fe8bebf323227d8fd6e43
Kufo2018 <40725858+Kufo2018@users.noreply.github.com>
2020-07-01 20:15:28 -0400

    Initial Commit

---
+++ testpackage/TestOut.py
@@ -0,0 +1,54 @@
+import math
+
+listOfSequences = []
+
+
+# Reads FASTA file
+def read_file(file_path):
+    # Locates and reads FASTA file
+    with open(file_path) as file:
+        aligned_sequences = file.readlines()
+
+    # Filters raw list of aligned sequences
+    for line in aligned_sequences:
+        if line[0] != ">":
+            listOfSequences.append(line.rstrip('\n'))
+
+
+# Pairs sequences
+def pair_sequences(sequence_list):
+    for x in sequence_list:
+        for y in sequence_list:
+            calculate_jc(x, y)
+        print("\n")
+
+
+# Calculates JC
+def calculate_jc(sequence_a, sequence_b):
+    aligns_but_mismatched = 0
+    ungapped_sites = 0
+    values = []
+
+    # Determines the p value
+    for x, y in zip(sequence_a, sequence_b):
+
+        # Aligned but mismatched sites
+        if x != y and x != "-" and y != "-":
+            aligns_but_mismatched += 1
+
+        # Ungapped sites
+        if x != "-" and y != "-":
+            ungapped_sites += 1
+
+    p = aligns_but_mismatched / ungapped_sites
+
+    distance = round(-3 / 4 * math.log(1 - ((4 / 3) * p)), 4)
+
+    values.append(distance)
+
+    for z in values:
+        print(z, end=" ")
+
+
+read_file("8.1_AlignedDNA.fas")
+pair_sequences(listOfSequences)
